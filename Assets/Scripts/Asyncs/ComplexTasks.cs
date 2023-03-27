a08f65d93485ab11d697a2138099773fd5f45dfe
Mystic Quest <l1skou@gmail.com>
2023-03-27 20:45:59 +0300

    adds some dummy async tasks

---
+++ Assets/Scripts/Asyncs/ComplexTasks.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ComplexTasks : MonoBehaviour
+{
+    private void Awake()
+    {
+        RunTasks();
+    }
+
+    public async void RunTasks()
+    {
+        Debug.Log("Starting tasks...");
+        Task[] tasks = new Task[5];
+
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            int num = i;
+            tasks[i] = Task.Run(async () =>
+            {
+                System.Random rnd = new System.Random();
+                int delay = rnd.Next(500, 10000);
+                await Task.Delay(delay);
+                Debug.Log($"Task {num + 1} completed after {delay} ms.");
+            });
+        }
+
+        await Task.WhenAll(tasks);
+        Debug.Log("All tasks completed.");
+    }
+}
