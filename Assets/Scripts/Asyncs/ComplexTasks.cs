04d2939edb2d0786e1fb057151c44c6af4d20249
Mystic Quest <l1skou@gmail.com>
2023-03-28 22:04:15 +0300

    minor change

--- Assets/Scripts/Asyncs/ComplexTasks.cs
+++ Assets/Scripts/Asyncs/ComplexTasks.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -11,22 +12,23 @@ public class ComplexTasks : MonoBehaviour
 
     public async Task RunTasks()
     {
-        Debug.Log("Starting tasks...");
+        Debug.Log($"Starting tasks on thread {Thread.CurrentThread.ManagedThreadId}");
         Task[] tasks = new Task[5];
+        System.Random rnd = new();
 
         for (int i = 0; i < tasks.Length; i++)
         {
             int num = i;
+            int delay = rnd.Next(500, 10000);
             tasks[i] = Task.Run(async () =>
             {
-                System.Random rnd = new System.Random();
-                int delay = rnd.Next(500, 10000);
                 await Task.Delay(delay);
-                Debug.Log($"Task {num + 1} completed after {delay} ms.");
+                Debug.Log($"Task {num + 1}, completed after {delay} ms, on thread {Thread.CurrentThread.ManagedThreadId}.");
             });
         }
 
         await Task.WhenAll(tasks);
         Debug.Log("All tasks completed.");
     }
+
 }
